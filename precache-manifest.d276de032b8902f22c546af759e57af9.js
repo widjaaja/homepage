ebcb79d4e3665d72b6d76a947db5fb94344275da
adjieevil <adjieevil@gmail.com>
2020-02-08 00:08:56 +0700

    Updates

---
+++ precache-manifest.d276de032b8902f22c546af759e57af9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05b8b72a0f174578103ce420b6e38c00",
+    "url": "/homepage/index.html"
+  },
+  {
+    "revision": "710640740f9e1abf7068",
+    "url": "/homepage/static/css/main.456dd4c7.chunk.css"
+  },
+  {
+    "revision": "a7649e4bd5a4d456e708",
+    "url": "/homepage/static/js/2.4effeaaa.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/homepage/static/js/2.4effeaaa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "710640740f9e1abf7068",
+    "url": "/homepage/static/js/main.d2ce08cc.chunk.js"
+  },
+  {
+    "revision": "cea037458a234ac72420",
+    "url": "/homepage/static/js/runtime-main.66742f85.js"
+  },
+  {
+    "revision": "2c36d9c420b91b1c29a9524974d7e2cc",
+    "url": "/homepage/static/media/tailwindcss.2c36d9c4.svg"
+  }
+]);
