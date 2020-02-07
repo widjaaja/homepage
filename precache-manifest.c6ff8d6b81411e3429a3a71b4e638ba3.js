c21e7913ef506ab383f1497d5228452270e5e03d
adjieevil <adjieevil@gmail.com>
2020-02-08 00:13:15 +0700

    Updates

---
+++ precache-manifest.c6ff8d6b81411e3429a3a71b4e638ba3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d335318ed5c1df9cdc26b4adc9c0a7c",
+    "url": "/widjaaja.github.io/index.html"
+  },
+  {
+    "revision": "fcddab65bbfa8ba20bc4",
+    "url": "/widjaaja.github.io/static/css/main.456dd4c7.chunk.css"
+  },
+  {
+    "revision": "a7649e4bd5a4d456e708",
+    "url": "/widjaaja.github.io/static/js/2.4effeaaa.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/widjaaja.github.io/static/js/2.4effeaaa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fcddab65bbfa8ba20bc4",
+    "url": "/widjaaja.github.io/static/js/main.ab7448b9.chunk.js"
+  },
+  {
+    "revision": "7fd273081f60c7ae787f",
+    "url": "/widjaaja.github.io/static/js/runtime-main.38a335f4.js"
+  },
+  {
+    "revision": "2c36d9c420b91b1c29a9524974d7e2cc",
+    "url": "/widjaaja.github.io/static/media/tailwindcss.2c36d9c4.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/homepage/precache-manifest.d276de032b8902f22c546af759e57af9.js"
+  "/widjaaja.github.io/precache-manifest.c6ff8d6b81411e3429a3a71b4e638ba3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/homepage/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/widjaaja.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
